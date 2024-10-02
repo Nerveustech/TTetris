744f2270b5076086b24b05dd35d00b04f9301e23
AndreaPIZZA <167854007+AndreaPIZZA@users.noreply.github.com>
2024-10-02 23:29:15 +0200

    Delete Proggetto_TTetris/Proggetto_TTetris directory

--- Proggetto_TTetris/Proggetto_TTetris/TProjectGCCdebbug.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo Inizio compilazione del TerminalTetris!
-gcc Tengine_core.c -c -g
-gcc Tengine_scoreboard.c -c -g
-gcc TProject_main01.c Tengine_core.o Tengine_scoreboard.o -lcurses -lSDL2 -lSDL2_mixer -g -o TTetris
-echo compilazione finita, buon divertimento :D
